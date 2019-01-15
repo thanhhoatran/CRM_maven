36d78f09bea1a2561b15633090241448ea376daf
khacson105 <khacson105@gmail.com>
2019-01-15 19:56:28 +0700

    Son eidt3

---
+++ src/test/java/Tests/TC_EditLead_27_28_30_31_32_33_34_35.java
@@ -0,0 +1,214 @@
+package Tests;
+
+import Actions.CRMEditLead_action;
+import Actions.CRMLogin_action;
+import Actions.CRMSearchLead_action;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class TC_EditLead_27_28_30_31_32_33_34_35 {
+    WebDriver driver;
+    String email = "tokyoken@gmail.com";
+    String pass = "15091992h";
+    String text = "abcd";
+    String textname = "van luat";
+    String emailChange = "123123";
+    String phoneChange = "qwe123";
+    String emailExist = "tokyoken@gmail.com";
+    String textchangeaddress = "160 Dong Da";
+
+
+
+    @BeforeClass
+    public void setup(){
+        System.setProperty("webdriver.chrome.driver", ".\\src\\test\\Resources\\drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://crmweb.ddns.net");
+        System.out.println(driver.getTitle());
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+    }
+    @Test
+    //Summary:Verify that "Edit Customer" page displays after searching then clicking on Edit button
+    // of each result in "Customer List"table and User can edit customer successfully with valid data
+    public void TC_Edit_27(){
+        //Preconditions: "Customer" page is opened
+        CRMLogin_action.enterEmail_Pass(driver,email,pass);
+        CRMLogin_action.clickButtonLogin(driver);
+        //Step1: Enter a name or a part of name into "Name" field
+        CRMSearchLead_action.enterSearchName(driver,textname);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        //Step2: Click on any "Edit" button of "Customer List" table
+        driver.findElement(By.xpath("//a[contains(.,'Edit')]")).click();
+        //Step3: Enter valid data into all field
+        CRMSearchLead_action.deleteAddress(driver);
+        CRMSearchLead_action.enterChangeAddress(driver,textchangeaddress);
+        //Step4: Click on "Save" button
+        CRMSearchLead_action.clickBtnSave(driver);
+        //Step5: Observed "Customer Information" form
+        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+        WebElement changeAddress = driver.findElement(By.xpath("//span[contains(.,'"+textchangeaddress+"')]"));
+        Assert.assertTrue(changeAddress.isDisplayed());
+        //BUG: Missing "Edit" column is located on the right "Phone" column in "Customers List" table
+    }
+    @Test
+    // Summary: Verify that "The email is not valid (ex: abc@abc)" message displays above "Email" field
+    // when User edit customer information in "Edit Customer" page with invalid email
+    public void TC_Edit_28()  {
+        //Preconditions: "Customer" page is opened
+        CRMLogin_action.enterEmail_Pass(driver,email,pass);
+        CRMLogin_action.clickButtonLogin(driver);
+        CRMSearchLead_action.enterSearchName(driver,textname);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//a[contains(.,'"+textname+"')]")).click();
+        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+        CRMEditLead_action.clickBtnEdit(driver);
+
+        //Step1: Enter invalid email into "Email" field
+        CRMEditLead_action.deleteEmail(driver);
+        CRMEditLead_action.enterEmail(driver,emailChange);
+        //Step2: Click on "Save" button
+        CRMEditLead_action.clickBtnSave(driver);
+        //Expected Result: "The email is not valid (ex: abc@abc)" message displays above "Email" field
+        WebElement mssEmail = driver.findElement(By.xpath("//span[contains(.,'The email is not valid (ex: abc@abc)')]"));
+        Assert.assertTrue(mssEmail.isDisplayed());
+    }
+    @Test
+    // Summary: Verify that "Only numbers 0-9" message displays above "Phone" field
+    // when User edit customer information in "Edit Customer" page with invalid phone numbers
+    public void TC_Edit_30()  {
+        //Preconditions: "Customer" page is opened
+        CRMLogin_action.enterEmail_Pass(driver,email,pass);
+        CRMLogin_action.clickButtonLogin(driver);
+        CRMSearchLead_action.enterSearchName(driver,textname);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//a[contains(.,'"+textname+"')]")).click();
+        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+        CRMEditLead_action.clickBtnEdit(driver);
+
+        //Step1:  Enter invalid data into "Phone" filed
+        //(E.G: qwe123)
+        CRMEditLead_action.deletePhone(driver);
+        CRMEditLead_action.enterPhone(driver,phoneChange);
+        //Step2: Click on "Save" button
+        CRMEditLead_action.clickBtnSave(driver);
+        //Expected Result: "Only numbers 0-9" message displays above "Phone" field
+        WebElement mssEmail = driver.findElement(By.xpath("//span[contains(.,'Only numbers 0-9')]"));
+        Assert.assertTrue(mssEmail.isDisplayed());
+    }
+    @Test
+    //Summary: Verify that "Please enter your name" message displays above "Name" field
+    // when User leaving "Name" field is blank in "Edit Customer" page
+    public void TC_Edit_31()  {
+        //Preconditions: "Customer" page is opened
+        CRMLogin_action.enterEmail_Pass(driver,email,pass);
+        CRMLogin_action.clickButtonLogin(driver);
+        CRMSearchLead_action.enterSearchName(driver,textname);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//a[contains(.,'"+textname+"')]")).click();
+        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+        CRMEditLead_action.clickBtnEdit(driver);
+
+        //Step1: Delete data into "Name" field
+        CRMEditLead_action.deleteName(driver);
+        //Step2: Click on "Save" button
+        CRMEditLead_action.clickBtnSave(driver);
+        //Expected Result: "Please enter your name" message displays above "Name" field
+        WebElement mssEmail = driver.findElement(By.xpath("//span[contains(.,'Please enter your name')]"));
+        Assert.assertTrue(mssEmail.isDisplayed());
+    }
+    @Test
+    //Summary: Verify that "Please enter your email" message displays above "Email" field
+    // when User leaving "Email" field is blank in "Edit Customer" page
+    public void TC_Edit_32()  {
+        //Preconditions: "Customer" page is opened
+        CRMLogin_action.enterEmail_Pass(driver,email,pass);
+        CRMLogin_action.clickButtonLogin(driver);
+        CRMSearchLead_action.enterSearchName(driver,textname);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//a[contains(.,'"+textname+"')]")).click();
+        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+        CRMEditLead_action.clickBtnEdit(driver);
+
+        //Step1: Delete data into "Email" field
+        CRMEditLead_action.deleteEmail(driver);
+        //Step2: Click on "Save" button
+        CRMEditLead_action.clickBtnSave(driver);
+        //Expected Result: "Please enter your email" message displays above "Email" field
+        WebElement mssEmail = driver.findElement(By.xpath("//span[contains(.,'Please enter your email')]"));
+        Assert.assertTrue(mssEmail.isDisplayed());
+    }
+    @Test
+    //Summary: Verify that "Please enter your phone" message displays above "Phone" field
+    // when User leaving "Phone" field is blank in "Edit Customer" page
+    public void TC_Edit_33()  {
+        //Preconditions: "Customer" page is opened
+        CRMLogin_action.enterEmail_Pass(driver,email,pass);
+        CRMLogin_action.clickButtonLogin(driver);
+        CRMSearchLead_action.enterSearchName(driver,textname);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//a[contains(.,'"+textname+"')]")).click();
+        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+        CRMEditLead_action.clickBtnEdit(driver);
+
+        //Step1: Delete data into "Phone" field
+        CRMEditLead_action.deletePhone(driver);
+        //Step2: Click on "Save" button
+        CRMEditLead_action.clickBtnSave(driver);
+        //Expected Result: "Please enter your phone" message displays above "Phone" field
+        WebElement mssEmail = driver.findElement(By.xpath("//span[contains(.,'Please enter your phone')]"));
+        Assert.assertTrue(mssEmail.isDisplayed());
+    }
+    @Test
+    //Summary: Verify that  "Please enter your address" message displays above "Address" field
+    // when User leaving "Address" field is blank in "Edit Customer" page
+    public void TC_Edit_34()  {
+        //Preconditions: "Customer" page is opened
+        CRMLogin_action.enterEmail_Pass(driver,email,pass);
+        CRMLogin_action.clickButtonLogin(driver);
+        CRMSearchLead_action.enterSearchName(driver,textname);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//a[contains(.,'"+textname+"')]")).click();
+        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+        CRMEditLead_action.clickBtnEdit(driver);
+
+        //Step1: Delete data into "Address" field
+        CRMEditLead_action.deleteAddress(driver);
+        //Step2: Click on "Save" button
+        CRMEditLead_action.clickBtnSave(driver);
+        //Expected Result: "Please enter your address" message displays above "Address" field
+        WebElement mssEmail = driver.findElement(By.xpath("//span[contains(.,'Please enter your address')]"));
+        Assert.assertTrue(mssEmail.isDisplayed());
+    }
+    @Test
+    //Summary: Verify that "That email is taken.Try another" message displays above "Email" field
+    // when entering email exists into "Email" field on "Edit Customer" page
+    public void TC_Edit_35()  {
+        //Preconditions: "Customer" page is opened
+        CRMLogin_action.enterEmail_Pass(driver,email,pass);
+        CRMLogin_action.clickButtonLogin(driver);
+        CRMSearchLead_action.enterSearchName(driver,textname);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//a[contains(.,'"+textname+"')]")).click();
+        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+        CRMEditLead_action.clickBtnEdit(driver);
+
+        //Step1: Enter a email exists into "Email" filed
+        CRMEditLead_action.deleteEmail(driver);
+        CRMEditLead_action.enterEmail(driver,emailExist);
+        //Step2: Click on "Save" button
+        CRMEditLead_action.clickBtnSave(driver);
+        //Expected Result: "That email is taken.Try another" message displays above "Email" field
+        WebElement mssEmail = driver.findElement(By.xpath("//span[contains(.,'That email is taken.Try another')]"));
+        Assert.assertTrue(mssEmail.isDisplayed());
+        //BUG: Missing "That email is taken.Try another" message displays above "Email" field
+        // when entering email exists into "Email" field on "Edit Customer" page
+    }
+}
--- src/test/java/Tests/TC_SearchLead_20_21_22_23_25.java
+++ src/test/java/Tests/TC_SearchLead_20_21_22_23_25.java
@@ -14,7 +14,7 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class TC_SearchLead_ {
+public class TC_SearchLead_20_21_22_23_25 {
     String email = "tokyoken@gmail.com";
     String pass = "15091992h";
     String text = "abcd";
@@ -24,55 +24,92 @@ public class TC_SearchLead_ {
     String textchangeaddress = "160 Dong Da";
     WebDriver driver;
     @BeforeClass
-    public void preCondion(){
+    public void setup(){
         System.setProperty("webdriver.chrome.driver", ".\\src\\test\\Resources\\drivers\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("http://crmweb.ddns.net");
         System.out.println(driver.getTitle());
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
-        CRMLogin_action.enterEmail_Pass(driver,email,pass);
-        CRMLogin_action.clickButtonLogin(driver);
+
     }
     @Test
+        //Summary:Verify that User can search customer information after entering valid data into "Search" textbox
     public void TC_SearchLead_20(){
+        //Preconditions: "Customer" page is opened
+        CRMLogin_action.enterEmail_Pass(driver,email,pass);
+        CRMLogin_action.clickButtonLogin(driver);
+        //Step1:Enter a name or a part of name into "Search" textbox
         CRMSearchLead_action.enterSearcal(driver,text);
+        //Step2: Press Enter keyboard
         driver.findElement(By.xpath(CRMSearchLead_page.txt_search)).sendKeys(Keys.ENTER);
+        //Expected Result: User can search customer information
         WebElement pageFail = driver.findElement(By.xpath("//h1[contains(.,'HTTP Status 404 - Not Found')]"));
         Assert.assertTrue(pageFail.isDisplayed());
     }
     @Test
+    //Summary: Verify that after User enters a name or a part of name into "Name" field of "Customer List" table, The related customers list is displayed.
     public void TC_SearchLead_21(){
+        //Preconditions: "Customer" page is opened
+        CRMLogin_action.enterEmail_Pass(driver,email,pass);
+        CRMLogin_action.clickButtonLogin(driver);
+        //Step1: Enter a name or a part of name into "Name" field
         CRMSearchLead_action.enterSearchName(driver,textname);
+        //Step2: Observed "Customer List" table
+        //Expected Result: The related customers list is displayed in "Customer List" table
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         WebElement name = driver.findElement(By.xpath("//a[contains(.,'"+textname+"')]"));
         Assert.assertTrue(name.isDisplayed());
     }
     @Test
+    //Summary: Verify that User can search customer by entering valid data into "Email" field of "Customer List" table
     public void TC_SearchLead_22(){
+        //Preconditions: "Customer" page is opened
+        CRMLogin_action.enterEmail_Pass(driver,email,pass);
+        CRMLogin_action.clickButtonLogin(driver);
+        //Step1: Enter valid email into "Email" field
         CRMSearchLead_action.enterSearchEmail(driver,textemail);
+        //Step2: Observed "Customer List" table
+        //Expected Result: The related customers list is displayed in "Customer List" table
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         WebElement email = driver.findElement(By.xpath("//td[contains(.,'"+textemail+"')]"));
         Assert.assertTrue(email.isDisplayed());
     }
     @Test
+    //Summary:Verify that User can search customer by entering valid data into "Address" field of "Customer List" table
     public void TC_SearchLead_23(){
+        //Preconditions: "Customer" page is opened
+        CRMLogin_action.enterEmail_Pass(driver,email,pass);
+        CRMLogin_action.clickButtonLogin(driver);
+        //Step1: Enter valid data into "Address" field
         CRMSearchLead_action.enterSearchAddress(driver,textaddress);
+        //Step2: Observed "Customer List" table
+        //Expected Result: The related customers list is displayed in "Customer List" table
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         WebElement address = driver.findElement(By.xpath("//td[contains(.,'"+textaddress+"')]"));
         Assert.assertTrue(address.isDisplayed());
     }
     @Test
+    //Summary:Verify that "Edit Customer" page displays after searching then clicking any customer name of result in "Customer List"table and User can edit customer successfully with valid data
     public void TC_SearchLead_25() {
+        //Preconditions: "Customer" page is opened
+        CRMLogin_action.enterEmail_Pass(driver,email,pass);
+        CRMLogin_action.clickButtonLogin(driver);
+        //Step1: Enter a name or a part of name into "Name" field
         CRMSearchLead_action.enterSearchName(driver,textname);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        //Step2: Click on any customer name in  "Name" column
         driver.findElement(By.xpath("//a[contains(.,'"+textname+"')]")).click();
         driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+        //Step3: Click on "Edit" button of "Customer Information" form
         CRMSearchLead_action.clickBtnEdit(driver);
         driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+        //Step4: Enter valid data into all field (e.g: Change address)
         CRMSearchLead_action.deleteAddress(driver);
         CRMSearchLead_action.enterChangeAddress(driver,textchangeaddress);
+        //Step5: Click on "Save" button
         CRMSearchLead_action.clickBtnSave(driver);
+        //Step6: Observed "Customer Information" form
         driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
         WebElement changeAddress = driver.findElement(By.xpath("//span[contains(.,'"+textchangeaddress+"')]"));
         Assert.assertTrue(changeAddress.isDisplayed());
