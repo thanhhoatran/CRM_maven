1e501c4d731332d4c1e3f9c04c287bdb67b7daba
thanhhoatran <thanhhoatrann114@gmail.com>
2019-01-23 15:09:27 +0700

    init_hoa

--- src/test/java/Tests/TC_EditLead_27_28_30_31_32_33_34_35.java
+++ src/test/java/Tests/TC_EditLead_27_28_30_31_32_33_34_35.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
